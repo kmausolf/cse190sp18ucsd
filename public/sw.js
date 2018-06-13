8638fd406537a0e4b1402d895b4d2c0c921668a6
kmausolf <kmausolf@ucsd.edu>
2018-06-13 13:58:57 -0700

    Removed cache entry cap

--- public/sw.js
+++ public/sw.js
@@ -25,8 +25,6 @@ if (workbox) {
       cacheName: 'image-cache',
       plugins: [
         new workbox.expiration.Plugin({
-          // Cache only 20 images
-          maxEntries: 20,
           // Cache for a maximum of a week
           maxAgeSeconds: 7 * 24 * 60 * 60,
         })
